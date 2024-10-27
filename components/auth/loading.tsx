6708bc4ef570119a9bf9cc2395f68082c07c91c0
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-27 19:35:07 +0800

    improve the loading components

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,13 +4,20 @@ import Image from "next/image";
 export default function Loading() {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
-      <Image
-        src="/logo.png"
-        width={120}
-        height={120}
-        alt="Logo"
-        className="animate-pulse duration-700"
-      />
+      <div className="flex items-center gap-0">
+          <Image 
+            src="/logo.png"
+            width={120}
+            height={120}
+            alt="Logo"
+            className="animate-pulse duration-700"
+          />
+           <h1 className="bg-gradient-to-r from-pink-500 via-orange-400 to-yellow-300 
+             inline-flex tracking-tight font-extrabold text-transparent 
+             bg-clip-text text-6xl sm:text-8xl animate-pulse duration-700">
+            uScape
+          </h1>
+      </div>
     </div>
   );
 };

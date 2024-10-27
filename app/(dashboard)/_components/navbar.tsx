49d2856334654b34a46324c7f6592d0e61a43ae9
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-27 21:18:27 +0800

    item list componensts

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { UserButton } from "@clerk/nextjs";
+
 export const Navbar = () => {
     return (
         <div className="flex items-center gap-x-4 p-5">
+            <div className="hidden lg:flex lg:flex-1">
+                Search
+            </div>
+
+            <div>
+                <UserButton/>
+            </div>
             
         </div>
     )

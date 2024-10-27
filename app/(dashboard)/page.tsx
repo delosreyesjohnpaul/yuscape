2a6248b7c321a513b48b489e0c4348de983646a5
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-27 20:20:24 +0800

    sidebar orgsidebar navbar skeleton

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            Dashboard export def
+        </div>
+    );
+};
+
+export default DashboardPage;

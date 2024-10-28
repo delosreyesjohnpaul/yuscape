894d35c97fe86dc6e121a6bff988afe33f8bb4da
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-28 20:26:39 +0800

    NewBoardButtonProps

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -9,6 +9,7 @@ import { EmptyBoards } from "./empty-boards";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptySearch } from "./empty-search";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
     orgId: string;
@@ -24,7 +25,16 @@ export const BoardList = ({ orgId, query } : BoardListProps) => {
     if (data === undefined) {
         return (
             <div>
-                Loading...
+                <h2 className="text-3xl">
+                {query.favourites ? "Favourite Boards" : "Team boards"}
+                </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId} disabled />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                </div>
             </div>
         )
     }
@@ -55,6 +65,7 @@ export const BoardList = ({ orgId, query } : BoardListProps) => {
             </h2>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+            <NewBoardButton orgId={orgId}/>
             {data?.map((board) => (
                 <BoardCard
                     key={board._id}

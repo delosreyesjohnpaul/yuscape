b9cb91e017effc106f2928f6bc06de4d3779699e
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-30 14:27:29 +0800

    fin text

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -3,6 +3,8 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
+import { Ellipse } from "./ellipse";
+import { Text } from "./text";
 
 interface LayerPreviewProps {
     id: string;
@@ -17,6 +19,26 @@ export const LayerPreview = memo(({ id, onLayerPointerDown, selectionColor }: La
     if (!layer) return null;
 
     switch (layer.type) {
+        case LayerType.Text:
+            return (
+              <Text
+                id={id}
+                layer={layer}
+                onPointerDown={onLayerPointerDown}
+                selectionColor={selectionColor}
+              />
+            );
+
+        case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+
         case LayerType.Rectangle:
             return (
                 <Rectangle
